47d2de8e0a7ffa2479f52748970bd838ceeb3676
VT <marfat.vt@gmail.com>
2016-10-12 19:45:22 +0100

    Modified view

---
+++ src/components/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul id="nav-items">
+  <li v-for:></li>
+
+  </ul>
+</template>
+
+<script>
+import { updateActiveNote } from '../vuex/actions'
+
+export default {
+  data () {
+    return {
+      show: 'all'
+    }
+  },
+  vuex: {
+    getters: {
+      notes: state => state.notes,
+      activeNote: state => state.activeNote
+    },
+    actions: {
+      updateActiveNote
+    }
+  },
+  computed: {
+    filteredNotes () {
+      if (this.show === 'all'){
+        return this.notes
+      } else if (this.show === 'favorites') {
+        return this.notes.filter(note => note.favorite)
+      }
+    }
+  }
+}
+</script>
